7dda6788783bbaad6c94f243ee74d34ecac18f9f
jaures <jaures@users.noreply.github.com>
2018-04-06 10:50:05 -0700

    Added Water Testing Project Code
    Added Water testing arduino code and Python Script

---
+++ poisoner.py
@@ -0,0 +1,35 @@
+#! /bin/env python
+
+import serial   # Needed for serial Communication
+import time     # Needed for sleep function
+import smtplib  # Needed for emailing
+#import imaplib  # Needed for emailing (receiving)
+
+## MODFIY THIS SECTION ##
+usr = "cs101f17@gmail.com"      # Your Username
+psw = "learning4lyfe"           # Your Password
+rcp = "jauresade@gmail.com"     # The recipient
+msg = "Poisoned!!!"   # The Message to Send
+com = "COM3"                   # The Com your Arduino Is on
+## END OF MODIFY SECTION ##
+
+msg_sent = 1
+
+ser = serial.Serial(com, 9600, timeout=1)
+while 1:
+    try:
+        # Read the Distance from Serial as an int
+        cnt = ser.readline();
+        print(cnt)
+        if("Poisoned" in cnt ) and msg_sent is 0:
+            server = smtplib.SMTP_SSL("smtp.gmail.com",465)
+            server.login(usr, psw)
+            server.sendmail(usr, rcp, msg)
+            server.close()
+            print("MSG SENT!")
+            time.sleep(1)
+        msg_sent = (msg_sent + 1) % 20 # Used to limit number of messages sent at once
+
+    except :#ser.SerialTimeoutException:
+        print('Data could not be read')
+        time.sleep(1)
---
+++ waterTester.ino
@@ -0,0 +1,24 @@
+int readPin = A7;
+int initRead = 0;
+double thresh = 1.05;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  initRead = analogRead(readPin);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Initial: ");
+  Serial.println(initRead);
+  Serial.print("Current Value: ");
+  Serial.println(analogRead(readPin));
+
+  while( analogRead(readPin) > initRead * thresh)
+  {
+    Serial.println("Poisoned!!");
+    delay(5000);
+  }
+  delay(100);
+}
